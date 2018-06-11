e0472ae1887ba9bf4c1f609db464715c8997ba90
chrisaor <pchpch0070@gmail.com>
2018-06-11 12:24:38 +0900

    model 작성 및 migrate

--- app/education/models.py
+++ app/education/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class School(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+
+
+class Student(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField('생성 일시', default=timezone.now)
+
+
