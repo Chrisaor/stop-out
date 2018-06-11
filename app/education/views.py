30c11db6cf0f8b8e7a9c579dc061df1df6f902ae
chrisaor <pchpch0070@gmail.com>
2018-06-11 13:00:55 +0900

    school detail 간단한 연결

--- app/education/views.py
+++ app/education/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 
+from education.models import School
+
 
 def index(request):
     return render(request, 'education/index.html')
 
 def school_list(request):
-    return render(request, 'education/school_list.html')
+    schools = School.objects.all()
+    context = {
+        'schools':schools,
+    }
+    return render(request, 'education/school_list.html', context)
 
-def school_detail(request):
-    return render(request, 'education/school_detail.html')
+def school_detail(request, pk):
+    school = School.objects.get(id=pk)
+    context = {
+        'school':school
+    }
+    return render(request, 'education/school_detail.html', context)
 
 def student_list(request):
     return render(request, 'education/student_list.html')
