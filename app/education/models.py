01823935e9c1c35dc38ba59598a997e40a95d567
chrisaor <pchpch0070@gmail.com>
2018-06-11 12:32:34 +0900

    model __str__추가 및 학생, 학교 인스턴스 생성

--- app/education/models.py
+++ app/education/models.py
@@ -6,10 +6,14 @@ class School(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 
 class Student(models.Model):
     school = models.ForeignKey(School, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     created_at = models.DateTimeField('생성 일시', default=timezone.now)
 
-
+    def __str__(self):
+        return self.name
